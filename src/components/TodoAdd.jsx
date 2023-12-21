7067e2db7b1cd3da41a982603a72129df0ae2cb3
avamys106 <avamys106@naver.com>
2023-12-21 16:05:47 +0900

    feat: react-toto add, count, update 기능추가

---
+++ src/components/TodoAdd.jsx
@@ -0,0 +1,33 @@
+import { useRef } from 'react';
+import styles from './TodoAdd.module.css';
+
+export default function TodoAdd({ todos, setTodos }) {
+  const inputRef = useRef(null);
+  function handleAddTodo() {
+    if (inputRef.current.value.trim()) {
+      setTodos([...todos, { id: todos.length, text: inputRef.current.value, done: false }]);
+    }
+    inputRef.current.value = '';
+  }
+  // console.log(todos);
+
+  function handleEnter(e) {
+    if (e.key === 'Enter') {
+      handleAddTodo();
+    }
+  }
+  return (
+    <div className={styles.todo_add}>
+      <input
+        onKeyUp={handleEnter}
+        ref={inputRef}
+        type="text"
+        placeholder="할 일을 입력하세요"
+        title="할 일을 입력하세요"
+      />
+      <button type="button" onClick={handleAddTodo}>
+        Add
+      </button>
+    </div>
+  );
+}
